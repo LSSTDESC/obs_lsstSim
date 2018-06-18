2fba7557c7c94d54fca17034c96ffa971c6ddda7
Heather Kelly <heather@slac.stanford.edu>
2018-06-18 09:14:09 -0400

    setting doAttachTransmissionCurve False
    See: https://github.com/LSSTDESC/obs_lsstSim/pull/16#issuecomment-398039106

--- config/assembleCoadd.py
+++ config/assembleCoadd.py
@@ -4,4 +4,4 @@ from lsst.utils import getPackageDir
 # Load configs shared between assembleCoadd and makeCoaddTempExp
 config.load(os.path.join(getPackageDir("obs_lsstSim"), "config", "coaddBase.py"))
 
-config.doAttachTransmissionCurve = True
+config.doAttachTransmissionCurve = False
